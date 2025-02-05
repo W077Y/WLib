0130885075164f9c8fed2cd6c8f8564eb52a7efa
Wolfgang Schütz <wolfgang.schuetz@anton-paar.com>
2025-02-05 13:52:18 +0100

    added Shared_Memory_Provider_Interface; moves Storage into its own file; adden mirrow_storage_t

--- Memory/inc/wlib-memory.hpp
+++ Memory/inc/wlib-memory.hpp
@@ -19,20 +19,6 @@ namespace wlib
       virtual void read(std::size_t add, std::span<std::byte> data)        = 0;
     };
   }    // namespace memory
-
-  namespace storage
-  {
-    template <typename T> class Non_Volatile_Storage_Interface
-    {
-    public:
-      using value_type = std::remove_cvref_t<T>;
-
-      virtual ~Non_Volatile_Storage_Interface() = default;
-
-      virtual value_type load() const            = 0;
-      virtual void       save(value_type const&) = 0;
-    };
-  }    // namespace storage
 }    // namespace wlib
 
 #endif    // WLIB_MEMORY_INTERFACE_HPP_INCLUDED
