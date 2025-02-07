ce8c2e607b54e14220a878ddd9f1c0eb7e9e4f7c
Wolfgang Schütz <WSchuetz@live.com>
2025-02-07 13:20:40 +0100

    added Shared_Resource_Provider

--- Provider/inc/wlib-Provider_Interface.hpp
+++ Provider/inc/wlib-Provider_Interface.hpp
@@ -53,6 +53,51 @@ namespace wlib
     std::span<std::byte> m_memory;
   };
 
+  template <typename T> class Shared_Resource_Provider_Interface
+  {
+  public:
+    using resource_t       = T;
+    using const_resource_t = const T;
+
+    class resource_token_t
+    {
+    public:
+      resource_token_t(Shared_Resource_Provider_Interface& provider, resource_t& resource)
+          : m_pro(provider)
+          , m_res(resource)
+      {
+        this->m_pro.lock();
+      }
+      resource_token_t(resource_token_t const&)            = delete;
+      resource_token_t(resource_token_t&&)                 = delete;
+      resource_token_t& operator=(resource_token_t const&) = delete;
+      resource_token_t& operator=(resource_token_t&&)      = delete;
+
+      ~resource_token_t() { this->m_pro.unlock(); }
+
+      resource_t&       get() & { return this->m_res; }
+      const_resource_t& get() const& { return this->m_res; }
+
+    private:
+      Shared_Resource_Provider_Interface& m_pro;
+      resource_t&                         m_res;
+    };
+
+    Shared_Resource_Provider_Interface(T& resource)
+        : m_resource{ resource }
+    {
+    }
+    virtual ~Shared_Resource_Provider_Interface() = default;
+
+    resource_token_t request() { return resource_token_t{ *this, this->m_resource }; }
+
+  private:
+    friend resource_token_t;
+    virtual void lock()   = 0;
+    virtual void unlock() = 0;
+    T&           m_resource;
+  };
+
 }    // namespace wlib
 
 #endif
