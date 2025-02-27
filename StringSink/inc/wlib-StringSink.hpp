a12523defff9fe73b12c8404ea8b4faab37a502f
Wolfgang Schütz <wolfgang.schuetz@anton-paar.com>
2025-02-27 18:23:43 +0100

    added StringBuilder;

--- StringSink/inc/wlib-StringSink.hpp
+++ StringSink/inc/wlib-StringSink.hpp
@@ -1,8 +1,10 @@
-#pragma once
+﻿#pragma once
 #ifndef WLIB_STRINGSINK_HPP_INCLUDED
 #define WLIB_STRINGSINK_HPP_INCLUDED
 
+#include <cstddef>
 #include <cstdint>
+#include <string_view>
 #include <wlib-Publisher.hpp>
 
 namespace wlib
@@ -16,7 +18,8 @@ namespace wlib
     virtual ~StringSink_Interface() = default;
 
     virtual bool operator()(char const* c_str);
-    virtual bool operator()(char const* c_str, uint32_t len) = 0;
+    virtual bool operator()(char const* c_str, std::size_t len) = 0;
+    virtual bool operator()(std::string_view str);
   };
 }    // namespace wlib
 
